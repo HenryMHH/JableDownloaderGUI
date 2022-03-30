f5717cff44e61845bf40d1bbbdb40d42b433b6f3
HenryHo <minghang.h@gmail.com>
2022-03-31 00:12:30 +0800

    decode not working yet

--- src/service/downloadService.ts
+++ src/service/downloadService.ts
@@ -55,9 +55,8 @@ export class DownloadService {
 			this._URI = _URI
 			this._IV = _IV
 			let fullURI = m3u8_url.split('/').slice(0, -1).join('/') + '/' + _URI
-			const _URIContent = await this.Axios.get(fullURI, { responseType: 'arraybuffer' })
+			const _URIContent = await this.Axios.get(fullURI)
 			downloadInfo.URIContent = _URIContent.data
-			console.log(_URIContent.data)
 
 			downloadInfo._IV = _IV
 			tsFileArray = __m3u8_content__.data
@@ -75,11 +74,19 @@ export class DownloadService {
 		const result = await this.Axios.get(url)
 	}
 
-	decrypt(data, IV, key: Uint8Array) {
-		let j = CryptoJS.enc.Utf8.parse(IV)
-		let t = key.toString()
-		let decrypt = AES.decrypt(data, t, { iv: j, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.NoPadding })
-		let c = decrypt.toString()
-		return c
+	decrypt(data, IV, key) {
+		// let j = CryptoJS.enc.Utf8.parse(IV)
+		// let t = key.toString()
+		// let decrypt = AES.decrypt(data, t, { iv: j, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.NoPadding })
+		// let c = decrypt.toString()
+		// return c
+		let _IV = Buffer.from(IV, 'utf8')
+		let _key = Buffer.from(key, 'utf8')
+		let plainbytes = Buffer.from(data, 'utf8')
+		let cipher = crypto.createDecipheriv('aes-128-cbc', _key, _IV)
+		let decrypt = cipher.update(plainbytes)
+		decrypt = Buffer.concat([decrypt, cipher.final()])
+
+		return decrypt
 	}
 }
